45dc8dc9f6ae7eaf4735912bc7b018a6796b54a6
ALEXWHISPER <15196608305@163.com>
2018-05-01 19:50:52 +0800

    upload, current feature: player move, enemy spawning, shoot, power up skill, skill UI

---
+++ Assets/Space Shooter Template FREE/Scripts/Player/PlayerSkills.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class PlayerSkills : MonoBehaviour {
+
+    public Button btn_PowerUp;
+    public Button btn_SkillB;
+    public Button btn_SkillC;
+
+    public event Action Event_PowerUp;
+    public event Action Event_SkillB;
+    public event Action Event_SkillC;
+
+    private void OnEnable() {
+        //  add btns onclick listeners
+        btn_PowerUp.onClick.AddListener(FireSkill_PowerUp);
+        btn_SkillB.onClick.AddListener(FireSkill_B);
+        btn_SkillC.onClick.AddListener(FireSkill_C);
+    }
+
+    private void OnDisable() {
+        //  remove btns onclick listeners
+        btn_PowerUp.onClick.RemoveListener(FireSkill_PowerUp);
+        btn_SkillB.onClick.RemoveListener(FireSkill_B);
+        btn_SkillC.onClick.RemoveListener(FireSkill_C);
+    }
+
+    private void FireSkill_PowerUp() {
+        if (Event_PowerUp != null) {
+            Event_PowerUp();
+        }
+    }
+
+    private void FireSkill_B() {
+        if (Event_SkillB != null) {
+            Event_SkillB();
+        }
+    }
+
+    private void FireSkill_C() {
+        if (Event_SkillC != null) {
+            Event_SkillC();
+        }
+    }
+}
---
+++ Assets/Space Shooter Template FREE/Scripts/Skills/Skill_PowerUp.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class Skill_PowerUp : SkillController {
+
+    public event Action<int> OnFireSkillWithLevel_PowerUp;
+
+    protected override void Start() {
+        base.Start();
+        curSkillLevel = 1;
+    }
+
+    private void OnEnable() {
+        playerSkillControl.Event_PowerUp += OnFireSkill;
+    }
+
+    private void OnDisable() {
+        playerSkillControl.Event_PowerUp -= OnFireSkill;
+    }
+
+    protected override void OnFireSkill() {
+        //  if the skill is cooling
+        if (this.isCooling) {
+            Debug.Log("Skill is Cooling...");
+            return;
+        }
+        
+        //  if the level has reached to the top value
+        if (++curSkillLevel > maxSkillLevel) {
+            Debug.Log("skill level has reached the top value");
+            return;
+        }
+
+        //  do something on firing skill
+        if (OnFireSkillWithLevel_PowerUp != null) {
+            OnFireSkillWithLevel_PowerUp(this.curSkillLevel);
+        }
+
+        //  scale up the level img
+        skillImg_Level.GetComponent<RectTransform>().localScale += new Vector3(0.1f, 0.1f, 0.1f);
+
+        //  cool down the skill
+        this.Cooling();
+    }
+
+    protected override void Cooling() {
+        base.Cooling();
+    }
+}
